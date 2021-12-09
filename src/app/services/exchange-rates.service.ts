182f58500c2c9dd43a9725133a125b5091a46ee7
NareshRobertson <naresh.nagandla@robertsoncollege.ca>
2021-12-09 16:43:38 -0600

    Uploading yhe currency converter application into Github

---
+++ src/app/services/exchange-rates.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ExchangeRatesResponse } from './payloads/exchange-rates-response';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExchangeRatesService {
+
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  getRates(base: string): Observable<ExchangeRatesResponse> {
+    return this.http.get<ExchangeRatesResponse>(`https://api.exchangerate.host/latest?source=ecb&base=${base}`);
+  }
+}
